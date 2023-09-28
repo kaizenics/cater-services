8b4616438753537881748ca67fe52a273ad0a9b6
Niko <nicosejohnsoriano@gmail.com>
2023-09-28 23:01:08 +0800

    Added SignupForm

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Landing from './pages/Landing'
 import About from './pages/About'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
+import SignupForm from './pages/SignupForm'
 import './App.scss';
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <Route path="/Home" element={<Home/>}/>
       <Route path="/Login" element={<Login/>}/>
       <Route path="/Signup" element={<Signup/>}/>
+      <Route path="/SignupForm" element={<SignupForm/>}/>
       <Route path="/About" element={<About/>}/>
     </Routes>
    </>
