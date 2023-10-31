c5f5328830ed7b1b1bd9e80966e91e5072f44b34
Niko <nicosejohnsoriano@gmail.com>
2023-10-31 23:06:12 +0800

    admin sidebar added

---
+++ src/components/AdminSidebar.js
@@ -0,0 +1,31 @@
+import { Link } from 'react-router-dom';
+import { BsHouseDoor, BsGear } from 'react-icons/bs';
+import icon from '../images/ate-gangs.png'
+import '../styles/AdminSidebar.scss';
+
+export default function SideNavbar() {
+  return (
+    <div className="sidebar-container">
+     <nav className="side-navbar">
+      <div className="home-title">
+        <img src={icon} className="side-icon" alt="Icon" />
+        
+      </div>
+      <ul className="sidebar-menu">
+        <li>
+          <Link to="/AdminPanel">
+            <BsHouseDoor className="sidebar-icon" />
+            Dashboard
+          </Link>
+        </li>
+        <li>
+          <Link to="/Settings">
+            <BsGear className="sidebar-icon" />
+            Settings
+          </Link>
+        </li>
+      </ul>
+    </nav>
+    </div>
+  );
+}
