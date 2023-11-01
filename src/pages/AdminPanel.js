8b77502d6c617939c0c138385b55bf1ac595412d
Niko <nicosejohnsoriano@gmail.com>
2023-11-02 01:18:16 +0800

    admin panel configuration

--- src/pages/AdminPanel.js
+++ src/pages/AdminPanel.js
@@ -1,4 +1,5 @@
 import AdminSidebar from "../components/AdminSidebar";
+import MenuItems from '../components/MenuItems';
 import { BiDish, BiDollarCircle, BiUser } from "react-icons/bi";
 import "../styles/AdminPanel.scss";
 
@@ -35,38 +36,21 @@ export default function Admin() {
           </section>
 
           <section className="crud-dishes">
-  <div className="crud-opt">
-    <h2>Menu Items</h2>
-    <table className="crud-table">
-      <thead>
-        <tr>
-          <th>Item Name</th>
-          <th>Price</th>
-          <th>Action</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>Pasta</td>
-          <td>$10.99</td>
-          <td>
-            <button className="edit-btn">Edit</button>
-            <button className="delete-btn">Delete</button>
-          </td>
-        </tr>
-        <tr>
-          <td>Pizza</td>
-          <td>$12.99</td>
-          <td>
-            <button className="edit-btn">Edit</button>
-            <button className="delete-btn">Delete</button>
-          </td>
-        </tr>
-        {/* Add more rows as needed */}
-      </tbody>
-    </table>
-  </div>
-</section>
+          <div className="crud-opt">
+              <h2>Menu Items</h2>
+              <table className="crud-table">
+                <thead>
+                  <tr>
+                    <th>Item Name</th>
+                    <th>Price</th>
+                    <th>Quantity</th>
+                    <th>Action</th>
+                  </tr>
+                </thead>
+                <MenuItems />
+              </table>
+            </div>
+          </section>
         </div>
       </div>
     </>
