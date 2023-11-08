36ea6d4110c75232298690d05d143c8811afabed
wachitorico <90526207+wachitorico@users.noreply.github.com>
2023-11-09 01:45:04 +0800

    About added
    Co-Authored-By: Niko <kzncs.dev@gmail.com>

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,66 @@
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+import img2 from "../images/ategangsred.png";
+import img3 from "../images/locationpic.png";
+import '../styles/About.scss';
 
 export default function About() {
     return (
         <>
-        <Navbar />
-        <Footer />
+            <Navbar />
+            <section>
+            <div className="mid-description">
+            <div className="desc-1">
+              <div className="text-content1-about">
+                <h1>About Us</h1>
+    
+              </div>
+            </div>
+            </div>
+            </section>
+            
+            <section>
+          <div className="mid-description">
+            <div className="desc-1">
+              <div className="text-content-about">
+                <h1>Ate Gang’s Catering Services </h1>
+                <p>
+                Ate Gang's Catering Services is a local catering business in Panabo 
+                that offers diverse daily offerings and door-to-door deliveries. 
+                With an increasing demand for efficient and high-quality catering services, 
+                enabling AGCS to make informed decisions and deliver exceptional service to its customers. 
+                With a focus on quality control and customer satisfaction, AGCS is dedicated to adapting to 
+                modern technologies and staying competitive in the market.
+                </p>
+              </div>
+              
+              <div className="img-container">
+                <img src={img2} alt="" />
+              </div>
+            </div>
+          </div>
+
+          <div className="mid-description">
+            <div className="desc-1">
+              <div className="img-container">
+                <img src={img3} alt="" />
+              </div>
+              <div className="text-content-about">
+                <h1>We're Located Here!</h1>
+                    <div className="text-content1-about">
+                        <h3>Panabo, Davao Del Norte, <br/>Open Every Weekdays<br/> 8:00AM - 5:00PM!!</h3>
+                  </div>
+            
+                
+              </div>
+            </div>
+          </div>
+
+        </section>
+            <Footer />
         </>
+        
+       
     )
 }
+
