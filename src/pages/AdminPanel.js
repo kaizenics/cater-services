7cded73988daf52f6c53da26d609c560e625482e
Niko <nicosejohnsoriano@gmail.com>
2023-11-21 00:22:23 +0800

    total dish, revenue, users updated

--- src/pages/AdminPanel.js
+++ src/pages/AdminPanel.js
@@ -6,6 +6,29 @@ import "../styles/AdminPanel.scss";
 
 export default function Admin() {
  const [menu, setMenu] = useState([]);
+ const [totalOrderedDishes, setTotalOrderedDishes] = useState(0);
+ const [totalRevenue, setTotalRevenue] = useState(0);
+ const [totalUsers, setTotalUsers] = useState(0);
+
+ useEffect(() => {
+  // Fetch Total Ordered Dishes
+  fetch("http://localhost/serverside/admin/totalDishes.php")
+    .then((response) => response.json())
+    .then((data) => setTotalOrderedDishes(data.totalOrderedDishes))
+    .catch((error) => console.error("Error:", error));
+
+  // Fetch Total Revenue
+  fetch("http://localhost/serverside/admin/totalRevenue.php")
+    .then((response) => response.json())
+    .then((data) => setTotalRevenue(data.totalRevenue))
+    .catch((error) => console.error("Error:", error));
+
+  // Fetch Total Users
+  fetch("http://localhost/serverside/admin/totalUsers.php")
+    .then((response) => response.json())
+    .then((data) => setTotalUsers(data.totalUsers))
+    .catch((error) => console.error("Error:", error));
+}, []);
 
  useEffect(() => {
   fetch("http://localhost/serverside/items/getItem.php")
@@ -25,21 +48,21 @@ export default function Admin() {
                 <h2>Total Ordered Dishes</h2>
                 <div className="stats-desc-1">
                   <BiDish className="dish-icon"></BiDish>
-                  <h1>1,117</h1>
+                  <h1>{totalOrderedDishes}</h1>
                 </div>
               </div>
               <div className="stats-box">
                 <h2>Total Revenue</h2>
                 <div className="stats-desc-1">
                   <BiDollarCircle className="dish-icon"></BiDollarCircle>
-                  <h1>100,000</h1>
+                  <h1>{totalRevenue}</h1>
                 </div>
               </div>
               <div className="stats-box">
                 <h2>Total Users</h2>
                 <div className="stats-desc-1">
                   <BiUser className="dish-icon"></BiUser>
-                  <h1>124</h1>
+                  <h1>{totalUsers}</h1>
                 </div>
               </div>
             </div>
